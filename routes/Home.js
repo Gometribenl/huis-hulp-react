20648fda0551d17ef9f0c2628becaa6b95812074
Zamira <zegbers@hotmail.com>
2020-06-02 13:52:54 +0200

    routes

---
+++ routes/Home.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import { Actions } from 'react-native-router-flux';
+import Routes from "../Routes";
+
+export default class Home extends React.Component {
+
+    render() {
+        return (
+
+
+                    <Text>This is HOME!</Text>
+
+
+
+        );
+    }
+}
+
+// const Home = () => {
+//   const goToAbout = () => {
+//         Actions.about()
+//     }
+//     return (
+
+//     )
+// }
+// export default Home
