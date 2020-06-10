60d8e389a90918ebec7a3394484dae99d1a97820
Bettina Kok <bettina@Bettinas-MacBook-Pro.local>
2020-06-10 11:27:23 +0200

    Register and login final css and axios

--- routes/Login.js
+++ routes/Login.js
@@ -11,6 +11,9 @@ import {
     Text,
 } from 'react-native-elements';
 import {Actions} from 'react-native-router-flux';
+import axios from 'axios';
+
+
 
 const styles = StyleSheet.create({
     container: {
@@ -47,7 +50,30 @@ const styles = StyleSheet.create({
 
     }
 });
+
 const image = { uri: "https://images.unsplash.com/photo-1589705436822-720a68b246fb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80" };
+
+constructor(props) {
+    super(props);
+
+    axios.get('http://10.0.2.2:8000/user/user/' + this.state.id)
+        .then(res => {
+            const nameList = res.data;
+            this.setState({ userData: nameList.data[0]});
+        })
+        .catch((error) => {
+            console.error(error)
+        })
+
+    axios.get('http://10.0.2.2:8000/user/chores/' + this.state.id)
+        .then(res => {
+            const choreList = res.data;
+            this.setState({ chores: choreList.data })
+        })
+}
+
+
+
 export default class Login extends Component {
     render() {
         return (
